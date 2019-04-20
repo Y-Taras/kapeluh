dc255df460e4bfa2ca2d813832350a4993be52e4
Taras Yaremkiv <tyare@softserveinc.com>
2019-04-20 17:55:29 +0300

    remove redundant column in price

--- src/js/price.js
+++ src/js/price.js
@@ -1,17 +1,47 @@
-document.addEventListener('DOMContentLoaded', function(){
-  if (window.location.pathname.includes("price")) {
-    fetch(
-      "https://spreadsheets.google.com/feeds/cells/1fpm1AjYJInjZJTjffAqJVmnI3Ju_AvNc8FTj-AREehg/1/public/full?alt=json"
-    )
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(data) {
-        let array = data.feed.entry;
-        let lastCell = array[array.length - 1]['gs$cell'];
-        const _entry = data.feed.entry.map(item => item.content['$t']);
-        console.log('Список: ', { _entry });
-        console.log('Розмір таблиці: рядків: ', lastCell.row,  'стовпців: ', lastCell.col );
-      });
-  }
-}, false);
+
+const googleSheetLink =
+  'https://spreadsheets.google.com/feeds/cells/1fpm1AjYJInjZJTjffAqJVmnI3Ju_AvNc8FTj-AREehg/1/public/full?alt=json';
+
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    if (window.location.pathname.includes("price")) {
+      fetch(googleSheetLink)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          let array = data.feed.entry;
+          let lastCell = array[array.length - 1]["gs$cell"];
+          const _entry = data.feed.entry.map(item => item.content["$t"]);
+          console.log("Список: ", {_entry});
+          console.log(
+            "Розмір таблиці: рядків: ",
+            lastCell.row,
+            "стовпців: ",
+            lastCell.col
+          );
+        });
+
+/*      // a function to build a list
+      let makeTemplate = function (data) {
+        let newList = "";
+        data.forEach(function (element) {
+          newList += `<li>${element}</li>`;
+        });
+        return newList;
+      };
+
+      // build a container template
+      let template = `  <div class="row">
+
+  </div><ul>
+                        ${makeTemplate()}
+                      </ul>`;*/
+
+      // add the template to the page
+      // document.querySelector('.price-table').innerHTML = template;
+    }
+  },
+  false
+);
