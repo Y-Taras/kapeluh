bf7504e4b5b9fc89f57b790bd449c08cc393eb83
y-taras <ytaras@gmail.com>
2020-06-08 20:03:38 +0300

    move script from price template to index js

--- src/js/index.js
+++ src/js/index.js
@@ -30,30 +30,52 @@ if (analytics) {
   console.log('Google Analytics has been disabled');
 }
 
-document.addEventListener(
-  "DOMContentLoaded",
-  function () {
-    //Bootstrap navbar hide on click outside
-    document.addEventListener('click', evt => {
-      const isNavbarTogglerActive = evt.target && evt.target.classList.contains('navbar-toggler');
-      const isMobileMenuOpen = document.getElementsByClassName("navbar-collapse")[0].classList.contains("show");
-      if (isMobileMenuOpen === true && !isNavbarTogglerActive) {
-        document.querySelector("button.navbar-toggler").click();
-      }
-    })
-
-    //Add white border to current tag
-    if (window.location.pathname) {
-      const activeCssClass = 'header__item--active'
-      let path = window.location.pathname;
-      if (path.startsWith('/article')) {
-        path = '/articles.html'
-      }
-      if (path.startsWith('/wiki')) {
-        path = '/wiki.html'
-      }
-
-      const currentAnchor = document.querySelector(`a[href$='${path}']`)
-      currentAnchor.classList.add(activeCssClass);
+document.addEventListener('DOMContentLoaded', function () {
+  //Bootstrap navbar hide on click outside
+  document.addEventListener('click', evt => {
+    const isNavbarTogglerActive = evt.target && evt.target.classList.contains('navbar-toggler');
+    const isMobileMenuOpen = document
+      .getElementsByClassName('navbar-collapse')[0]
+      .classList.contains('show');
+    if (isMobileMenuOpen === true && !isNavbarTogglerActive) {
+      document.querySelector('button.navbar-toggler').click();
     }
   });
+
+  //Add white border to current tag
+  if (window.location.pathname) {
+    const activeCssClass = 'header__item--active';
+    let path = window.location.pathname;
+    if (path.startsWith('/article')) {
+      path = '/articles.html';
+    }
+    if (path.startsWith('/wiki')) {
+      path = '/wiki.html';
+    }
+
+    const currentAnchor = document.querySelector(`a[href$='${path}']`);
+    currentAnchor.classList.add(activeCssClass);
+  }
+
+  //Insert current date into price page header
+  if (window.location.pathname.includes('price')) {
+    const d = new Date();
+    const month = [
+      'січня',
+      'лютого',
+      'березня',
+      'квітня',
+      'травня',
+      'червня',
+      'липня',
+      'серпня',
+      'вересня',
+      'жовтня',
+      'листопада',
+      'грудня'
+    ];
+    const currentDate = [d.getDate()] + ' ' + month[d.getMonth()] + ' ' + d.getFullYear() + ' р.';
+
+    document.querySelector('.price-date').innerText = 'ЦІНИ Актуальні станом на ' + currentDate;
+  }
+});
